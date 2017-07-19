eeb09b290ed3abb4d211132d8a30c97d5dc21bca
Samuel Barbosa <Samuel Barbosa>
2017-07-18 22:08:38 -0300

    altera sol lista 01 ex 06

--- Lista_01/Exercicio_06.m
+++ Lista_01/Exercicio_06.m
@@ -2,33 +2,34 @@ diary 'Exercicio_06.txt' % salva o output deste script em arquivo texto
 disp('EXERCÍCIO 06')
 
 disp('Itens 1 a 3 -------------------------------------------------------')
-[X, Y] = meshgrid(linspace(0,2.5,10), linspace(0,2.5,10));
-Y(Y > 2.5 - X) = 0;
-X(X > 2.5 - Y) = 0;
-f = X .* Y
+disp('Como f é estritamente crescente em x e y, a restição será ativa.')
+disp('Logo a solução se dará sobre a reta y = 2.5 - x.')
 
-k = find(f >= max(max(f)));
-x_opt = X(k)
-y_opt = X(k)
-f_opt = max(max(f))
+x = linspace(0,2.5,10);
+y = 2.5 - x;
+f = x .* y
+
+k = find(f >= max(f));
+x_opt = x(k)
+y_opt = y(k)
+f_opt = max(f)
 
 disp('Itens 4 e 5 -------------------------------------------------------')
 for t = [50, 100, 200, 500, 1000]
     tic
-    [X, Y] = meshgrid(linspace(0,2.5,t),linspace(0,2.5,t));
-    Y(Y > 2.5 - X) = 0;
-    X(X > 2.5 - Y) = 0;
-    f = X .* Y;
+    x = linspace(0,2.5,t);
+    y = 2.5 - x;
+    f = x .* y;
 
-    k = find(f >= max(max(f)));
+    k = find(f >= max(f));
     fprintf('t=%d .....................................................',t)
-    x_opt = X(k)
-    y_opt = X(k)
-    f_opt = max(max(f))
+    x_opt = x(k)
+    y_opt = y(k)
+    f_opt = max(f)
     time = toc
     
     % Item 5
     f_star = 25/16;
-    ef = 1 / ((f_star - f_opt) * time)
+    ef = ((f_star - f_opt) * time) ^ (-1)
 end
 diary off
