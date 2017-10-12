d366f62677adf14dd560ceb24778ac809c396837
Samuel Barbosa <samuelbarbosaa@gmail.com>
2017-10-12 12:43:25 -0300

    ex 1

--- Macro_III/Lista_01/exercicio_1.m
+++ Macro_III/Lista_01/exercicio_1.m
@@ -14,51 +14,48 @@ alpha_1 = 1/15;
 alpha_2 = -1/600;
 
 n = 1000;
-w = linspace(0, 20, n)';
+w = linspace(wmin, wmax, n)';
+V = ones(n, 1); % chute inicial para a função valor
+G = ones(n, 1); % chute inicial para a função política
 
-V = zeros(n, 1);
-G = V;
-
-tol = 10^-5;
-itmax = 3000;
-iter = 1;
+% inicia variaveis do algoritmo de iteracao
 err = 1;
+tol = 10^-5;
+itmax = 2000;
+iter = 1;
 
-% integracao numerica
-x = linspace(wmin, wmax, n);
-h = x(2) - x(1);
-E = @(w, V, n) V' * ( f(w, alpha_1, alpha_2) ./ sum(f(w, alpha_1, alpha_2)) );
+% fdp discretizada e funcao valor esperado
+fw = f(w, alpha_1, alpha_2) ./ sum(f(w, alpha_1, alpha_2));
+E = @(fw, V, n) V' * fw; 
 
+% algoritmo de iteracao
 while err > tol && iter < itmax
-    N = u(b, gamma) + beta * E(w, V, n);
+    N = u(b, gamma) + beta * E(fw, V, n);
     N = repmat(N, n, 1);
-    A = u(w, gamma) + beta * ( (1-pi) * V + pi * N );
- 
+    A = u(w, gamma) + beta * ((1-pi) * V + pi * N);
     [TV, G] = max([N A], [], 2);
-
-    err = max(abs(TV - V));
+    err = abs(max(TV - V));
     V = TV;
-    fprintf('Iteração: %d, Desvio: %1.7f \n', iter, err)
     iter = iter + 1;
 end
 
-G = G - 1;
+G = G-1;
+R = min(w(G == 1));
+R
 
 subplot(2,1,1);
-plot(x, V);
+plot(w, V);
 title('Funcao valor');
 ylabel('v(w)');
 xlabel('w');
 
 subplot(2,1,2);
-plot(x, G);
+plot(w, G);
 title('Funcao politica');
 ylabel('g(w)');
 xlabel('w');
 
 
-R = min(x(G == 1));
-R
 
 %% (ii)
 
@@ -70,36 +67,33 @@ itmax = 3000;
 iter = 1;
 err = 1;
 
-% integracao numerica
-E = @(w, V, n) V' * ( f(w, alpha_1, alpha_2) ./ sum(f(w, alpha_1, alpha_2)) );
+% fdp discretizada e funcao valor esperado
+fw = f(w, alpha_1, alpha_2) ./ sum(f(w, alpha_1, alpha_2));
+E = @(fw, V, n) V' * fw; 
 
+% algoritmo de iteracao
 while err > tol && iter < itmax
-    N = u(b, gamma) + beta * E(w, V, n);
+    N = u(b, gamma) + beta * E(fw, V, n);
     N = repmat(N, n, 1);
-    A = u(w, gamma) + beta * ( (1-pi) * V + pi * N );
- 
+    A = u(w, gamma) + beta * ((1-pi) * V + pi * N);
     [TV, G] = max([N A], [], 2);
-
-    err = max(abs(TV - V));
+    err = abs(max(TV - V));
     V = TV;
-    fprintf('Iteração: %d, Desvio: %1.7f \n', iter, err)
     iter = iter + 1;
 end
 
-G = G - 1;
+G = G-1;
+R = min(w(G == 1));
+R
 
 subplot(2,1,1);
-plot(x, V);
+plot(w, V);
 title('Funcao valor');
 ylabel('v(w)');
 xlabel('w');
 
 subplot(2,1,2);
-plot(x, G);
+plot(w, G);
 title('Funcao politica');
 ylabel('g(w)');
 xlabel('w');
-
-
-R = min(x(G == 1));
-R
