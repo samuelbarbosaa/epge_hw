797dd2da73cfd9151517b5d24d4f9dcfb298d22a
Samuel Barbosa <samuelbarbosaa@gmail.com>
2017-10-13 23:02:02 -0300

    ex5

--- Macro_III/Lista_01/ex5/ex5.m
+++ Macro_III/Lista_01/ex5/ex5.m
@@ -1,18 +1,32 @@
 clc, clear all
 
+% Exercício 5
 
+% Dados do problema
 A = [5 -2 3;
     -3 9 1;
     2 -1 -7];
 
 b = [-1 2 3]';
 
-%Metodo de Jacobi
+%% Metodo de Jacobi
+x0 = zeros(size(b,1), 1); % Chute inicial
 
-x0 = zeros (size(b,1), 1);
+n = size(b,1);
+err = 1;
+tol = 10^-5;
+it = 1;
+itmax = 1000;
 
-function y = J(A, x, n)
-   d = diag(A);
-   r = A(n,:);
-   r = r([1:n-1 n+1:end]);
-   y = (1/d(n)) * (b(n) - 
+while err > tol && it < itmax
+    for i = 1:n
+      x(i) = (1/A(i,i))*(b(i) - A(i,[1:i-1, i+1:n]) * x0([1:i-1, i+1:n]));
+    end
+x1 = x';
+err = norm(x1 - x0, 1);
+it = it +1;
+end   
+disp('x1 =');
+disp(x1);
+
+% Eliminacao Gaussiana
