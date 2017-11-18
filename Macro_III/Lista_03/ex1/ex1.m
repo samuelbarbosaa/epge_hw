f1d4fd5fc8a80b6bdc4d871def51b93b3def6b17
Samuel Barbosa <samuelbarbosaa@gmail.com>
2017-11-18 17:54:48 -0200

    replace by ref

--- Macro_III/Lista_03/ex1/ex1.m
+++ Macro_III/Lista_03/ex1/ex1.m
@@ -1,86 +1,98 @@
-clear
-% 
-%    EXERCICIO 01
-% 
+clc, close all
+% clear all
 
-a_inf = input('Insira o limite inferior de crédito {-12, -10, -8, -6 ,-4 ou -2}: ');
+al = -2;
+  
+eh = 1;
+el = 0.1;
+pi_hh = 0.925;
+pi_lh = 0.5;
+beta = 0.99322;
+sigma = 1.5;
+ah = 20;
+ql = 0.5;
+qh = 1.5;
+nA = 300;
+nE = 2;
+alpha = 0.9;
+Y = [];
 
-%% Parametros e funcoes
-beta = 0.99332; sigma = 1.5; 
-u = @(c) (c .^ (1-sigma))/(1-sigma);
+PI = [pi_hh (1-pi_hh); pi_lh (1-pi_lh)];
+A = linspace(al,ah,nA)';
+E = linspace(eh,el,nE);
+Al = A;
+[E A] = meshgrid(E,A);
+X = [E(:) A(:)];
 
-n_a = 150; step = 0.1; a_sup = a_inf + (n_a-1)*step; A = a_inf:step:a_sup;
-eh = 1; el = 0.1; q = 1; q_inf = 0; q_sup = 2; qp = 2;
+q = mean([ql, qh]);
 
-phh = 0.925; phl = 1 - phh;
-plh = 0.5; pll = 1 - plh;
+u = @(c)(c.^(1-sigma)/(1-sigma));
 
-z = 1; k = 1; kmax = 1000;
-while abs(z)> 10^-5 && abs(q - qp) > 10^-6 && k < kmax
+tol = 1e-2;
+maxit = 1000;
+max_it_excesso = 100;
+tol_excesso = 1e-2;
 
-%% Funcoes valor e politica (item a)
-Ch = max(eh + A - A' * q, 10^-5);
-Cl = max(el + A - A' * q, 10^-5);
-Vh = zeros(n_a, 1); Vl = Vh;
+it_excesso = 0;
+excesso = tol_excesso+1;
+while abs(excesso)>tol_excesso && it_excesso<max_it_excesso && abs(qh-ql)>1e-6
 
-it = 1; itmax = 1000; err = 1;
-while err > 10^-5 && it < itmax
-[TVh, gh] = max(u(Ch) + beta * (phh .* Vh + phl .* Vl));
-[TVl, gl] = max(u(Cl) + beta * (plh .* Vh + pll .* Vl));
+    C = repmat(A(:),1,nA) + repmat(E(:),1,nA) - repmat(Al',nE*nA,1)*q;
+    U = u(max(0,C));
+    V = zeros(nE*nA,1);
+    TV = V;
 
-err = max(max(abs(TVh - Vh')), max(abs(TVl - Vl')));
-Vh = TVh'; Vl = TVl';
-it = it+1;
+    Prob = kron(PI,ones(nA,1));
+
+    it = 0;
+    desv = tol+1;
+    while desv>tol && it<maxit
+        Vaux = U + beta*Prob*reshape(V,[nA,nE])';
+        [TV pos] = max(Vaux,[],2);
+        G = A(pos);
+        desv = max(max(abs(TV-V)));
+        V = TV;
+        it = it+1;
+    end
+    if it>=maxit, warning(['função valor não convergiu. Desvio foi de ' num2str(desv) ' para al = ' num2str(al)]); end
+
+    % Encontra a matriz M
+    M = zeros(nE*nA);
+    for i = 1:nE*nA
+        for j = 1:nE
+            M(i,pos(i)+(j-1)*nA) = PI(ceil(i/nA),j);
+        end
+    end
+
+    % Encontra a distribuição invariante de M
+    P_invar = invdist_numeric(M);
+
+    excesso = P_invar'*G;
+    disp([num2str(ql) ' ' num2str(q) ' ' num2str(qh) ' | ' num2str(excesso)]);
+    if excesso > 0;
+        ql = q;
+        q = (1-alpha)*qh + alpha*ql;
+    else
+        qh = q;
+        q = (1-alpha)*ql + alpha*qh;
+    end
+    it_excesso = it_excesso + 1;
+end
+Q = [Q q];
+
+if al == -2
+    X1 = reshape(V,[nA,nE]);
+    X2 = reshape(G,[nA,nE]);
+    figure; hold on;
+    plot(A(:,1),X1(:,1)); plot(A(:,1),X1(:,2)); grid on;
+    figure; hold on;
+    plot(A(:,1),X2(:,1)); plot(A(:,1),X2(:,2)); grid on;
 end
 
-%% Matriz M (item b)
-ih = zeros(size(Cl)); il = zeros(size(Cl));
-for j = 1:n_a
-    ih(gh(j), j) = 1;
-    il(gl(j), j) = 1;
-end
-        
-M = [phh*ih plh*il; 
-     phl*ih pll*il]';
-lambda = 1/n_a * ones(1, 2*n_a);
+P_numeric = invdist_numeric(M);
+P_analytic = invdist_analytic(M);
+disp(['A diferença máxima entre a dist. invariante obtida iterativamente '...
+    'e a obtida através de autovetor associado ao autovalor unitário é de: ']);
+disp(num2str(max(abs(P_analytic-P_numeric))));
 
-%% Distribuicao invariante como autovetor de M (item c)
-[eigvec, eigval] = eig(M');
-[row, col] = find(round(real(diag(eigval)))==1);
-lambda_eig = eigvec(:, col) / sum(eigvec(:, col));
-
-%% Distribuicao invariante como "ponto fixo" do operador linear M (item d)
-it = 1; err = 1; itmax = 1000;
-while err > 10^-5 && it < itmax
-lambda_prime = lambda * M;
-err = max(abs(lambda - lambda_prime));
-lambda = lambda_prime;
-it = it+1;
-end
-lambda = lambda/sum(lambda);
-
-%% Excesso de oferta de credito (item e)
-gh = A(gh); gl = A(gl);
-z = lambda * [gl gh]';
-
-if z > 10^-5
-    q_inf = q;
-elseif z < -10^-5
-    q_sup = q;
-end
-
-fprintf("z = %.8f, q = %.8f \n", z, q);
-qp = q;
-q = (q_inf + q_sup) / 2;
-k = k+1;
-end
-
-subplot(2,1,1); plot(A, TVh, A, TVl);
-legend('V(e_H, a)', 'V(e_L, a)', 'location', 'bestoutside');
-title('Funções valor nos estados e_H & e_L');
-
-subplot(2,1,2); plot(A, gh, A, gl);
-legend('g(e_H, a)', 'g(e_L, a)', 'location', 'bestoutside');
-title('Funções política nos estados e_H & e_L');
-
-
+figure; plot(AL,Q); grid on;
